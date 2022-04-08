755f53786675e245f6a22e63eb31ce5295d51ddd
Michal Stybowski <mstybowski@gmail.com>
2022-04-09 00:59:04 +0200

    Added newsletter signup action

--- src/sections/Newsletter.js
+++ src/sections/Newsletter.js
@@ -1,7 +1,57 @@
+import { toaster } from "evergreen-ui";
+import { useState } from "react";
+import auth from "../../auth";
+
 const Newsletter = (props) => {
+  const { credentials } = auth;
+  const Authorization = `Basic ${new Buffer.from(credentials).toString(
+    "base64"
+  )}`;
+
+  const [mail, setMail] = useState("");
+
+  async function newsletterSignupAction() {
+    const res = await fetch(`https://adchitects-cms.herokuapp.com/newsletter`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization,
+      },
+      body: JSON.stringify({ email: mail }),
+    });
+
+    const responseJSON = await res.json();
+
+    if (res.status === 200) {
+      toaster.success(responseJSON.message);
+    } else {
+      toaster.warning(responseJSON.message);
+    }
+
+    console.log(responseJSON);
+  }
+
   return (
     <>
-      <h1>Newsletter</h1>
+      <h1>Sign up for Newsletter</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          newsletterSignupAction();
+        }}
+      >
+        <label htmlFor="email">
+          Location
+          <input
+            id="email"
+            value={mail}
+            placeholder="Type your email"
+            onChange={(e) => setMail(e.target.value)}
+          />
+        </label>
+
+        <button>Submit</button>
+      </form>
       <h3>Type: {props.type}</h3>
     </>
   );
